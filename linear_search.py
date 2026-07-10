7fde488fdf97dad8f9481600c433a59a21cfbf38
divya2630 <192521401.simats@saveetha.com>
2026-07-10 14:37:40 +0530

    Create linear_search.py

---
+++ linear_search.py
@@ -0,0 +1,11 @@
+# Linear Search
+
+arr = [10, 25, 30, 45, 50]
+key = 30
+
+for i in range(len(arr)):
+    if arr[i] == key:
+        print("Key found at index", i)
+        break
+else:
+    print("Key not found")
